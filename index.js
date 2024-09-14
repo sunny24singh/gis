b62e3437ca78713438a9803e1b58880f9aaea124
sunny singh <sunny24singh@hotmail.com>
2024-09-15 00:37:28 +0530

    git commit -m "last section"

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
 
-let icon = document.querySelector("#icon"); 
+let icon = document.querySelector("#icon");
 icon.addEventListener("click" , () => {
     let icon = document.querySelector(".toggt");
     icon.classList.toggle("toggt2") 
 
     
+})
+
+
+let hover = document.querySelector(".image-container_section3");
+
+hover.addEventListener('mouseover' , () => {
+    let d = document.querySelector(".section3_datas2");
+    d.style.backgroundColor = "#0096FF"
+})
+
+hover.addEventListener('mouseout' , () => {
+    let d = document.querySelector(".section3_datas2");
+    d.style.backgroundColor = "black"
 })
