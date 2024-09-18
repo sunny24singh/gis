9698af24ff566037e2032eb50eab855b213f6baf
sunny singh <sunny24singh@hotmail.com>
2024-09-19 01:16:12 +0530

    git commit -m "dropdown add"

--- index.js
+++ index.js
@@ -12,11 +12,55 @@ icon.addEventListener("click" , () => {
 let hover = document.querySelector(".image-container_section3");
 
 hover.addEventListener('mouseover' , () => {
-    let d = document.querySelector(".section3_datas2");
-    d.style.backgroundColor = "#0096FF"
+
+   // let image = "url('https://eecenvironmental.com/wp-content/uploads/2017/08/GIS-1024x524.jpg')"
+    let d = document.querySelector("#section3_data_div1");
+    d.style.background = "rgba(0, 0, 0, 0.4)";
+   // d.style.height ="400px";
+   // d.style.width = "650px";
+   
+
+   
+     
+
+
+
+    
+    // d.style.opacity = "0.5"
 })
 
 hover.addEventListener('mouseout' , () => {
-    let d = document.querySelector(".section3_datas2");
-    d.style.backgroundColor = "black"
+    let d = document.querySelector("#section3_data_div1");
+    d.setAttribute("style" , "")
+})
+
+
+//dropdown
+
+let drop_btn = document.querySelector("#dropbtn");
+
+let drop_data1 = document.querySelector("#sec");
+
+drop_btn.addEventListener('mouseover' , () => { 
+    drop_data1.style.display = "flex"  
+
+    drop_data1.addEventListener('mouseover' , () => {
+        drop_data1.style.display = "flex"  
+    })
+})
+
+// setTimeout(() => {
+//     drop_btn.addEventListener('mouseout' , () => {
+//         drop_data1.style.display = "none"  
+//     }) 
+// }, 2000);
+
+
+
+
+
+drop_data1.addEventListener('mouseout' , () => {
+    
+
+    drop_data1.style.display = "none"  
 })
